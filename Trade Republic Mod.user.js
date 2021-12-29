b56b91b5e860d1003facc2ea5e9df01a70f9d73e
Tim Eßer <essertim13@googlemail.com>
2021-12-29 13:43:34 +0100

    removed custom darkmode

--- Trade Republic Mod.user.js
+++ Trade Republic Mod.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Trade Republic Mod
 // @namespace    http://tampermonkey.net/
-// @version      0.4
-// @description  Adds Darkmode, Converts $ to € in knockout's and disables auto logout on app.traderepublic.com
+// @version      0.5
+// @description  Converts $ to € in knockout's and disables auto logout on app.traderepublic.com
 // @author       123456687548
 // @match        *://app.traderepublic.com/*
 // @updateURL    https://github.com/123456687548/TradeRepublicWebMod/raw/master/Trade%20Republic%20Mod.user.js
@@ -16,7 +16,6 @@
 
 var style;
 var noLogoutTimer;
-var darkMode = true;
 var noLogout = false;
 
 var exchangeAPI = 'https://openexchangerates.org/api/latest.json?app_id=bb5d7092dbae49b3a7be12dd75508b72'
@@ -50,78 +49,6 @@ function processJSON_Response(data) {
 
 function reportAJAX_Error(rspObj) {
     console.error(`TM scrpt => Error ${rspObj.status}!  ${rspObj.statusText}`);
-}
-
-function addDarkMode() {
-    style = GM_addStyle(`
-    :root {
-        --color-background: #000;
-        --color-text-primary: #fff;
-        --color-text-secondary: #ffffff;
-        --color-tag-text: #000;
-        --color-banner-nav-link: #ffffff;
-        --color-banner-nav-focus: #fff;
-        --color-banner-nav-active: #fff;
-        --color-dropdown-list-text: #000000;
-        --color-dropdown-list-text-disabled: #505050;
-        --color-list-item-background-hover: #333333;
-        --color-button-secondary-background-hover: #444444;
-        --color-chart-text: #ffffff;
-        --color-chart-text-active: #ffffff;
-        --color-chart-reference-label-background: #000000;
-        --color-chart-label-border: #fff;
-        --color-chart-positive-performance-background: #000000;
-        --color-chart-negative-performance-background: #000000;
-        --color-tab-active: #fff;
-        --color-tab-default: #95a0a6;
-        --color-description-title: #ffffff;
-        --color-disclaimer-text: #ffffff;
-        --color-side-modal-background: #000;
-        --color-side-modal-heading: #fff;
-        --color-code-input-background: #333333;
-        --color-pin-input-background: #525252;
-        --color-content-overlay: hsl(0deg 0% 0% / 60%);
-        --color-chart-text-active-background: #000;
-        --color-search-hover-background: #767676;
-        --color-search-background: #303030;
-        --color-search-foreground: #ffffff;
-        --color-search-focused-background: #767676;
-        --color-search-drawer-background: #000;
-        --color-search-drawer-focused-background: #383838;
-        --color-table-column-header-background: #0e0e0e;
-        --color-table-column-header: #ffffff;
-        --color-table-row-hover: #0e0e0e;
-        --color-tag-background: #fff;
-    }
-
-    .alertBox.-warning {
-        background-color: #310000;
-    }
-
-    .-selected .dropdownList__optionName, .dropdownList__optionName.-withDesc {
-        color: #000;
-    }
-
-    .menuDotsIcon{
-        filter: invert(100%);
-    }
-
-    .current-time-tick text{
-        fill: #fff;
-    }
-
-    .autosuggestDrawer__footer {
-        background: #000;
-    }
-
-    .filterSection__header {
-        background-color: #0e0e0e;
-    }
-
-    .filtersHeader {
-        background-color: var(--color-background);
-    }
-`);
 }
 
 function convertCurrencyToEuro() {
@@ -180,22 +107,6 @@ function addEuroRow(appendTo) {
     appendTo.appendChild(dd);
 }
 
-function removeDarkMode() {
-    if (style !== undefined) {
-        style.remove();
-    }
-}
-
-function toggleDarkMode() {
-    if (darkMode) {
-        darkMode = false;
-        removeDarkMode();
-    } else {
-        darkMode = true;
-        addDarkMode();
-    }
-}
-
 function enableNoLogout() {
     noLogoutTimer = setInterval(function() {
         localStorage.setItem('lastActivity', Date.now());
@@ -220,20 +131,11 @@ var addCustomButtonsInterval = setInterval(function() {
     var navigationBar = document.getElementsByClassName("navigation__list");
     if (navigationBar.length == 0) return
 
-    var btnDarkMode = document.createElement('li');
-    btnDarkMode.innerHTML = '<input type="checkbox" id="darkModeBtn" name="darkModeBtn" value="darkModeBtn" checked="true"><label for="darkModeBtn" class="navigationItem__link">DarkMode</label>';
-    btnDarkMode.classList.add("navigationItem")
-
     var btnNoLogout = document.createElement('li');
     btnNoLogout.innerHTML = '<input type="checkbox" id="btnNoLogout" name="btnNoLogout" value="btnNoLogout"><label for="btnNoLogout" class="navigationItem__link">No Auto Logout</label>';
     btnNoLogout.classList.add("navigationItem")
 
-    navigationBar[0].appendChild(btnDarkMode);
     navigationBar[0].appendChild(btnNoLogout);
-
-    document.getElementById("darkModeBtn").addEventListener(
-        "click", toggleDarkMode, false
-    );
 
     document.getElementById("btnNoLogout").addEventListener(
         "click", toggleNoLogout, false
@@ -241,10 +143,6 @@ var addCustomButtonsInterval = setInterval(function() {
 
     clearInterval(addCustomButtonsInterval);
 }, 1000);
-
-if (darkMode) {
-    addDarkMode();
-}
 
 setInterval(function() {
     convertCurrencyToEuro();
